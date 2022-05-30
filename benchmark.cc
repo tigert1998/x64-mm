451626e1fdd3942cefe7a5c22a6b117c89f2bd27
tigertang <tigertang@zju.edu.cn>
2022-05-30 18:52:21 +0800

    Update benchmark

--- benchmark.cc
+++ benchmark.cc
@@ -7,27 +7,37 @@ int main() {
   std::chrono::high_resolution_clock::time_point t1 =
       std::chrono::high_resolution_clock::now();
   asm volatile(R"(
+vxorps %%ymm0, %%ymm0, %%ymm0
+vxorps %%ymm1, %%ymm1, %%ymm1
+vxorps %%ymm2, %%ymm2, %%ymm2
+vxorps %%ymm3, %%ymm3, %%ymm3
+vxorps %%ymm4, %%ymm4, %%ymm4
+vxorps %%ymm5, %%ymm5, %%ymm5
+vxorps %%ymm6, %%ymm6, %%ymm6
+vxorps %%ymm7, %%ymm7, %%ymm7
+vxorps %%ymm8, %%ymm8, %%ymm8
+vxorps %%ymm9, %%ymm9, %%ymm9
+
 loop:
 
+vfmadd231ps %%ymm0, %%ymm0, %%ymm0
+vfmadd231ps %%ymm1, %%ymm1, %%ymm1
+vfmadd231ps %%ymm2, %%ymm2, %%ymm2
+vfmadd231ps %%ymm3, %%ymm3, %%ymm3
+vfmadd231ps %%ymm4, %%ymm4, %%ymm4
+vfmadd231ps %%ymm5, %%ymm5, %%ymm5
+vfmadd231ps %%ymm6, %%ymm6, %%ymm6
+vfmadd231ps %%ymm7, %%ymm7, %%ymm7
+vfmadd231ps %%ymm8, %%ymm8, %%ymm8
+vfmadd231ps %%ymm9, %%ymm9, %%ymm9
+
 addl $-1, %0
-
-vfmadd231ps %%ymm0, %%ymm1, %%ymm8
-vfmadd231ps %%ymm1, %%ymm2 ,%%ymm9
-vfmadd231ps %%ymm2, %%ymm3, %%ymm10
-vfmadd231ps %%ymm3, %%ymm4, %%ymm11
-vfmadd231ps %%ymm4, %%ymm5, %%ymm12
-vfmadd231ps %%ymm5, %%ymm6, %%ymm13
-vfmadd231ps %%ymm6, %%ymm7, %%ymm14
-vfmadd231ps %%ymm7, %%ymm0, %%ymm15
-
-cmpl $0, %0
-
-jg loop
+jne loop
   )" ::"r"(t));
   std::chrono::high_resolution_clock::time_point t2 =
       std::chrono::high_resolution_clock::now();
 
-  int64_t flops_per_iter = 8 * 8 * 2;
+  int64_t flops_per_iter = 10 * 8 * 2;
   int64_t flops = flops_per_iter * t;
   double seconds =
       std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1)
